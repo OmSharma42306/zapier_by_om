9a70a21e47ba888bea7a09ca371765de83713e95
Om Sharma <omsharma.83173@gmail.com>
2025-01-31 04:19:17 +0530

    db-added-actions-schema

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -4,8 +4,9 @@ const PORT = 3000;
 const app = express();
 
 
-app.post("/hooks/",(req,res)=>{
-    
+app.post("/hooks/catch/:userId/:zapId",(req,res)=>{
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
 })
 
 
