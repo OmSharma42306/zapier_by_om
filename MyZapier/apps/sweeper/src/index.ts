d77463b48fc98d785e5397f673dcaa33ac3de88f
Om Sharma <omsharma.83173@gmail.com>
2025-08-06 00:36:33 +0530

    feat : added-sweeper-service-for-atomicity-across-services.

---
+++ MyZapier/apps/sweeper/src/index.ts
@@ -0,0 +1,45 @@
+// @ts-ignore
+import { prismaClient as client } from "@repo/db/client";
+// import {prismaClient as client } from "@repo/db/src/index";
+
+import { Queue } from "bullmq";
+
+// BullMq stuff
+const myQueue = new Queue('sweeper');
+
+async function runSweeper(){
+    
+    while(true){
+        
+        // pick things from db
+        const pendingRows = await client.zapRunOutbox.findMany({
+            where:{},
+            take : 10
+        });
+        console.log("Pending Rows",pendingRows);
+    
+        // put things to bull mq
+
+        pendingRows.map(async (r:any)=>{
+             await myQueue.add('zapProcess',{
+            zapId : r.zapRunId
+        })
+        });
+        
+        
+        // delete the entries in db.
+        
+        await client.zapRunOutbox.deleteMany({
+            where:{
+                id : {
+                    in: pendingRows.map((tr:any)=>tr.id)
+                },
+                }
+        });
+        
+        
+        
+    }
+}
+
+runSweeper();
