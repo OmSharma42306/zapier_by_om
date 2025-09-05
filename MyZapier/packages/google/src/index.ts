aebb22f72d1641d690d7bf5f66eb764b2ab20380
Om Sharma <omsharma.83173@gmail.com>
2025-09-05 18:22:41 +0530

    feat : added-refresh-token-also-for-fetching-drive-stuff.

--- MyZapier/packages/google/src/index.ts
+++ MyZapier/packages/google/src/index.ts
@@ -1,45 +1,7 @@
-import { oauth2Client } from "./oauthClient";
-import { google } from "googleapis";
-import { prismaClient as client } from "@repo/db"
-import { json } from "stream/consumers";
+import { GOOGLE_DOCS_ACTIONS,GOOGLE_DOCS_TRIGGERS } from "./google-docs/google-docs-utilities";
+import { syncGoogleDrive } from "./google-drive/syncGoogleDrive-service"
 
-export async function appendToGoogleDocs(userId : any,documentId : any,text : any){
-    
-  const googleAuth : any = await client.user.findFirst({select:{googleAuth:true},where:{id : userId}});
-  console.log("hello");
-  console.log("google Auth Tojjens",googleAuth);
-  if(!googleAuth) return;
-  let googleAuthToken ;
-  googleAuth.googleAuth.map((g:any)=>{
-      googleAuthToken =JSON.parse(g.allTokens) ;
-      console.log("htt",JSON.parse(g.allTokens));
-      console.log("set successfully");
-  })
-  
-    console.log("token at trigger",googleAuthToken);
-        
-    if (!googleAuthToken) return;
-  
-    oauth2Client.setCredentials(googleAuthToken);
-
-  try {
-    const docs = google.docs({ version: 'v1', auth: oauth2Client });
-    await docs.documents.batchUpdate({
-      documentId,
-      requestBody: {
-        requests: [
-          {
-            insertText: {
-              location: { index: 1 },
-              text: text + '\n',
-            },
-          },
-        ],
-      },
-    });
-
-    }catch(e){
-        console.error(e);
-    }
-
-}
+export const GOOGLE_DOCS = { GOOGLE_DOCS_ACTIONS,GOOGLE_DOCS_TRIGGERS };
+export const GOOGLE_SHEET = {};
+export const GMAIL = {};
+export const GOOGLE_DATA = { syncGoogleDrive }
