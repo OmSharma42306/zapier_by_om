e0e406c781c3bf662f62c59e6255195dc7a2c867
Om Sharma <omsharma.83173@gmail.com>
2025-10-01 17:57:32 +0530

    feat : added-google-data-info-api-service.

--- MyZapier/apps/primary-backend/src/index.ts
+++ MyZapier/apps/primary-backend/src/index.ts
@@ -4,6 +4,7 @@ import { userRouter } from "./routes/userRouter";
 import { zapRouter } from "./routes/zapRouter";
 import { triggerRouter } from "./routes/trigger";
 import { actionRouter } from "./routes/action"
+import { googleDataServiceRouter } from "./routes/google-data-service"
 const app = express();
 const PORT = 5000;
 
@@ -14,6 +15,7 @@ app.use('/api/v1/user',userRouter);
 app.use('/api/v1/zap',zapRouter);
 app.use('/api/v1/trigger',triggerRouter);
 app.use('/api/v1/action',actionRouter);
+app.use('/api/v1/googleData',googleDataServiceRouter)
 app.get('/',(req,res)=>{
     res.json({msg:"Hi! Welcome to Primary Backend"})
 })
