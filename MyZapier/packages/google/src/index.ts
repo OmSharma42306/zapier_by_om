d7ddd9000db64b96545fa4b0f4080129d768b697
Om Sharma <omsharma.83173@gmail.com>
2025-11-17 18:00:46 +0530

    feat(gmail-action): added full Send Email integration (subject, body, signature, labels)

--- MyZapier/packages/google/src/index.ts
+++ MyZapier/packages/google/src/index.ts
@@ -1,7 +1,8 @@
 import { GOOGLE_DOCS_ACTIONS,GOOGLE_DOCS_TRIGGERS } from "./google-docs/google-docs-utilities";
-import { syncGoogleDrive } from "./google-drive/syncGoogleDrive-service"
+import { syncGoogleDrive } from "./google-drive/syncGoogleDrive-service";
+import { GOOGLE_GMAIL_ACTIONS } from "./gmail/gmail-utilities"
 
 export const GOOGLE_DOCS = { GOOGLE_DOCS_ACTIONS,GOOGLE_DOCS_TRIGGERS };
 export const GOOGLE_SHEET = {};
-export const GMAIL = {};
+export const GMAIL = { GOOGLE_GMAIL_ACTIONS };
 export const GOOGLE_DATA = { syncGoogleDrive }
