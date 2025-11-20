7935f3ff9388cd71cb6d87d5d2c21038160c15e0
Om Sharma <omsharma.83173@gmail.com>
2025-11-20 22:16:55 +0530

    refactor(google-integration): use centralized googleTokenManager and remove inline DB/token logic from getGoogleDataInfo route

--- MyZapier/packages/google/src/googleTokenManager/googleTokenManager.ts
+++ MyZapier/packages/google/src/googleTokenManager/googleTokenManager.ts
@@ -1,7 +1,8 @@
 import { prismaClient as client} from "@repo/db";
 import { oauth2Client } from "../oauthClient"
 
-async function googleTokenManager(userId : number){
+export async function googleTokenManager(userId : number){
+    console.log(' i am here',userId);
     const googleAuth = await client.user.findMany({
         select : {googleAuth : true},
         where: {
@@ -9,20 +10,24 @@ async function googleTokenManager(userId : number){
         },
     });
 
+    console.log("gegg",googleAuth)
+
 
     let googleAuthToken : any;
 
     let googleAuthInfo = googleAuth[0].googleAuth[0];
-
+    console.log("info",googleAuthInfo);
     googleAuthToken  = googleAuthInfo.allTokens;
 
-        
+        console.log("pass 1")
     googleAuthToken = googleAuthInfo.allTokens;
     let expire_date= googleAuthToken?.expiry_date;
     const now = Date.now();
     const FIVE_MIN = 5 * 60 * 1000;
-
+  oauth2Client.setCredentials(googleAuthToken);
     if(expire_date - now < FIVE_MIN){
+            console.log("Refreshing token...");
+        console.log("pass 2")
         const { credentials }:any = await oauth2Client.refreshAccessToken();
 
 
@@ -32,11 +37,15 @@ async function googleTokenManager(userId : number){
                 allTokens : credentials
             }
         });
-
+        console.log("pass x")
         oauth2Client.setCredentials(credentials);
-        
-    }
+        console.log("pass y")
 
+    console.log("Token refreshed.");
+    }
+    console.log("info2",googleAuthInfo);
+console.log("pass 3")
+    return googleAuthInfo;
 
 
 
